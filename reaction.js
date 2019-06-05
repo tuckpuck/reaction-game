74cdb57ecf02c855ae8dc808b4ce24782a50eb2e
tuckpuck <tucker.triggs@yahoo.com>
2019-06-04 21:19:51 -0600

    formatted js file, smoothed out animation effect

--- reaction.js
+++ reaction.js
@@ -1,48 +1,46 @@
-function getRandomColor(){
-  var letters = '0123456789ABCDEF'.split('');
-  var color = '#';
-  for (var i=0; i<6; i++) {
-    color += letters[Math.round(Math.random()*15)];
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'.split('');
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
   }
   return color;
 }
 
-var clickedTime; var createdTime; var reactionTime;
+let clickedTime; let createdTime; let reactionTime;
 
 function makeBox() {
+  createdTime = Date.now();
 
-createdTime=Date.now()
+  let time = Math.random();
+  time *= 5000;
 
-var time=Math.random();
-time=time*5000;
+  setTimeout(() => {
+    if (Math.random() > 0.5) {
+      document.getElementById('box').style.borderRadius = '100px';
+    } else {
+      document.getElementById('box').style.borderRadius = '0px';
+    }
 
-setTimeout(function(){
+    let top = Math.random();
+    let left = Math.random();
 
-if (Math.random()>0.5) {
-  document.getElementById("box").style.borderRadius="100px";
-} else {
-   document.getElementById("box").style.borderRadius="0px";
+    top *= 300;
+    left *= 300;
+
+    document.getElementById('box').style.top = `${top}px`;
+    document.getElementById('box').style.left = `${left}px`;
+    document.getElementById('box').style.backgroundColor = getRandomColor();
+    document.getElementById('box').style.display = 'block';
+  }, time);
 }
 
-var top=Math.random();
-var left=Math.random();
-
-top=top*300;
-left=left*300;
-
-document.getElementById("box").style.top=top+"px";
-document.getElementById("box").style.left=left+"px";
-  document.getElementById("box").style.backgroundColor=getRandomColor();
-  document.getElementById("box").style.display="block";
-}, time);
-}
-
-document.getElementById("box").onclick=function() {
-  clickedTime=Date.now();
-  reactionTime=(clickedTime-createdTime)/1000;
-  document.getElementById("time").innerHTML=reactionTime;
-  this.style.display="none";
+document.getElementById('box').onclick = function () {
+  clickedTime = Date.now();
+  reactionTime = (clickedTime - createdTime) / 1000;
+  document.getElementById('time').innerHTML = reactionTime;
+  this.style.display = 'none';
   makeBox();
-}
+};
 
 makeBox();
